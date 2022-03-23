801bb2058c603ce6c183cc6112ebb34f9b093ffa
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-03-23 11:55:53 +0300

    jquery plugins

---
+++ services/services.js
@@ -0,0 +1,125 @@
+(function ($) {
+	'use strict';
+
+	var moneyMatters = {
+		/* ---------------------------------------------
+            ## Scroll top
+         --------------------------------------------- */
+		scroll_top: function () {
+			$('body').append(
+				"<a href='#top' id='scroll-top' class='topbutton btn-hide'><span class='fa fa-angle-double-up'></span></a>"
+			);
+			var $scrolltop = $('#scroll-top');
+			$(window).on('scroll', function () {
+				if ($(this).scrollTop() > $(this).height()) {
+					$scrolltop.addClass('btn-show').removeClass('btn-hide');
+				} else {
+					$scrolltop.addClass('btn-hide').removeClass('btn-show');
+				}
+			});
+			$("a[href='#top']").on('click', function () {
+				$('html, body').animate(
+					{
+						scrollTop: 0,
+					},
+					'normal'
+				);
+				return false;
+			});
+		},
+
+		mobile_menu: function () {
+			$('.site-navigation .mainmenu-wrap nav').meanmenu({
+				meanMenuClose: 'X',
+				meanMenuCloseSize: '18px',
+				meanScreenWidth: '992',
+				meanExpandableChildren: true,
+				meanMenuContainer: '.mobile-menu',
+			});
+			if ($('.menu.right-menu').length) {
+				var mobileLeftMenu = $(
+					'.site-header .navigation-area .mainmenu-wrap .menu.right-menu ul li'
+				)
+					.clone()
+					.appendTo('.mobile-menu .mean-bar .mean-nav ul');
+			}
+		},
+
+	
+		sticky_header: function () {
+			if ($('#sticky-header').length) {
+				var stickyMenu = $('.site-header').clone().appendTo('#sticky-header');
+				$(window).on('scroll', function () {
+					var w = $(window).width();
+					if (w > 992) {
+						if ($(this).scrollTop() > 350) {
+							$('#sticky-header').slideDown(500);
+						} else {
+							$('#sticky-header').slideUp(500);
+						}
+					}
+				});
+			}
+		},
+
+
+		nav_scroller: function () {
+			if ($('.site-header').length) {
+				$('.menu ul').navScroll({
+					mobileDropdown: true,
+					mobileBreakpoint: 991,
+					scrollSpy: true,
+					navHeight: 65,
+				});
+			}
+		},
+		
+		
+	modal_popup: function(){
+		event.preventDefault();
+		$(document).ready(function(){
+			$(".btn1").click(function(){
+				event.preventDefault()
+				$("#myModal").modal('show');
+			});
+		});
+	},
+	
+
+		/* ---------------------------------------------
+         function initializ
+         --------------------------------------------- */
+		initializ: function () {
+			deliciousaApp.scroll_top();
+			deliciousaApp.mobile_menu();
+			deliciousaApp.sticky_header();
+			deliciousaApp.nav_scroller();
+			deliciousaApp.modal_popup()
+		},
+	};
+
+
+	$(document).ready(function(){
+		$(".btn").click(function(){
+			event.preventDefault();
+			$("#myModal").modal('show');
+			
+		});
+	});
+
+
+	$(document).ready(function(){
+		$(".btn").click(function(){
+			event.preventDefault();
+			$("#myModal").modal('hide');
+			
+			
+		});
+	});
+	/* ---------------------------------------------
+     Document ready function
+     --------------------------------------------- */
+	$(function () {
+		deliciousaApp.initializ();
+	});
+})(jQuery);
